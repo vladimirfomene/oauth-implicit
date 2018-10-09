f383416d9954c4f88292ae99253e3596fb5fab43
Vladimir Fomene <vladimirfomene@gmail.com>
2018-10-09 18:04:28 +0100

    Create public and private endpoints for resource server

---
+++ src/main/java/com/example/oauthimplicit/AppController.java
@@ -0,0 +1,34 @@
+package com.example.oauthimplicit;
+
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class AppController {
+
+    @RequestMapping(value = "/api/public", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String publicEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a public endpoint! You don\'t need to be authenticated to see this.")
+                .toString();
+    }
+
+    @RequestMapping(value = "/api/private", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String privateEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a private endpoint! You need to be authenticated to see this.")
+                .toString();
+    }
+
+    @RequestMapping(value = "/api/private-scoped", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String privateScopedEndpoint() {
+        return new JSONObject()
+                .put("message", "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.")
+                .toString();
+    }
+}
