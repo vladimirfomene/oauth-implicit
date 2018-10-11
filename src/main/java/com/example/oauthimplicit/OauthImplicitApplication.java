22048c09acbc48e960b77905125fa5128d6631b9
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-11 12:24:22 -0300

    removing all the useless space;

--- src/main/java/com/example/oauthimplicit/OauthImplicitApplication.java
+++ src/main/java/com/example/oauthimplicit/OauthImplicitApplication.java
@@ -1,19 +1,11 @@
 package com.example.oauthimplicit;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class OauthImplicitApplication {
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(OauthImplicitApplication.class, args);
-	}
-
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(OauthImplicitApplication.class, args);
+    }
 }
